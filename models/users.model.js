8e047efc6972fc5a963310efaad8da846a9053cc
Lauri Niva <niva.lauri@gmail.com>
2023-04-07 09:36:40 +0300

    User Mongoose model

---
+++ models/users.model.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlenght: 4,
+  },
+  username: {
+    type: String,
+    required: true,
+    minlenght: 4,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlenght: 4,
+  },
+  website: {
+    type: String,
+    required: true,
+    minlenght: 4,
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlenght: 6,
+  },
+  address: {
+    street: { type: String, required: true },
+    suite: { type: String, required: true },
+    city: { type: String, required: true },
+    zipcode: { type: String, required: true },
+    geo: {
+      lat: { type: String, required: true },
+      lng: { type: String, required: true },
+    },
+  },
+  company: {
+    name: { type: String, required: true },
+    catchPhrase: { type: String, required: true },
+    bs: { type: String, required: true },
+  },
+});
+
+userSchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export default mongoose.model('User', userSchema);
