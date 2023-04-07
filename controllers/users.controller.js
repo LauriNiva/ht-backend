b749b76f386603c79569aceb3a888d71b06a456f
Lauri Niva <niva.lauri@gmail.com>
2023-04-07 09:58:45 +0300

    Käyttäjän päivitys

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -14,18 +14,17 @@ usersRouter.get('/', async (req, res) => {
 });
 
 usersRouter.post('/', async (req, res) => {
-  const {newUserData} = req.body;
+  const { newUserData } = req.body;
   try {
     const newUser = new User(newUserData);
     const createdUser = await newUser.save();
     res.json(createdUser);
   } catch (error) {
-    console.log('Error while creating new user',error);
+    console.log('Error while creating new user', error);
     //VIRHEEN LUKU JA OHJAUS
     res.status(500).json({ error: error });
   }
 });
-
 
 usersRouter.delete('/:id', async (req, res) => {
   const userId = req.params.id;
@@ -34,6 +33,19 @@ usersRouter.delete('/:id', async (req, res) => {
     res.status(204).end();
   } catch (error) {
     console.log('Error while trying to delete an user', error);
+    res.status(500).json({ error: error });
+  }
+});
+
+usersRouter.patch('/:id', async (req, res) => {
+  const userId = req.params.id;
+  console.log('userId', userId)
+  const { updatedUserData } = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, updatedUserData, {new: true})
+    res.json(updatedUser);
+  } catch (error) {
+    console.log('Error while trying to update an user', error);
     res.status(500).json({ error: error });
   }
 });
